c2837e1e0529ed467619f059cb9c15b29d6b8e99
Alexis Le Bourhis <lebourhisalexis11@gmail.com>
2023-05-24 17:00:33 +0200

    Add door and lever

---
+++ Assets/Lever.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    [SerializeField] GameObject _door;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
