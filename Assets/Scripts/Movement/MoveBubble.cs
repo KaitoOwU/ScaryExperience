a0290e8efe6f11ede7fdb2ccb920071e364b7012
Jujuazer <justinprat07@gmail.com>
2023-05-24 10:31:18 +0200

    Add - Movement avec courbe

--- Assets/Scripts/Movement/MoveBubble.cs
+++ Assets/Scripts/Movement/MoveBubble.cs
@@ -9,11 +9,21 @@ public class MoveBubble : MonoBehaviour
     Vector3 _movementDirection = new Vector3();
     bool _isMoving = false;
 
-    public GameObject joystickThumb;
-    public GameObject joystickBG;
-
     Vector3 _goToPosition;
     Vector2 _startPositionFinger;
+
+
+    [SerializeField] float _delayLerpMove;
+    [SerializeField] AnimationCurve _curveLerp;
+
+    private void OnEnable()
+    {
+        Etouch.EnhancedTouchSupport.Enable();
+    }
+    private void OnDisable()
+    {
+        Etouch.EnhancedTouchSupport.Disable();
+    }
 
     private void Awake()
     {
@@ -22,15 +32,37 @@ public class MoveBubble : MonoBehaviour
         Etouch.Touch.onFingerMove += Touch_onFingerMove;
         Etouch.Touch.onFingerDown += Touch_onFingerDown;
         Etouch.Touch.onFingerUp += Touch_onFingerUp;
+
+        _goToPosition = transform.position;
     }
 
-    private void Touch_onFingerUp(Etouch.Finger finger)
 
+    private IEnumerator MoveToPosition ()
     {
-        
+        float moveTimer = 0;
+        Vector3 startPos = transform.position;
 
+        while (moveTimer < _delayLerpMove)
+        {
+            moveTimer += Time.fixedDeltaTime;
+
+            //Debug.Log("prout : " + moveTimer / _delayLerpMove);
+            //Debug.Log(transform.position);
+            //Debug.Log(_goToPosition);
+
+            transform.position = Vector3.Lerp(startPos, _goToPosition, _curveLerp.Evaluate(moveTimer / _delayLerpMove));
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        _isMoving = false;
+    }
+
+    private void Touch_onFingerUp(Etouch.Finger finger) 
+    {
         Vector2 fingerTouchDelta = finger.screenPosition - _startPositionFinger;
-        Debug.Log("delta : " + fingerTouchDelta);
+        //Debug.Log("delta : " + fingerTouchDelta);
+
 
         //x
         if (Mathf.Abs(fingerTouchDelta.x) > Mathf.Abs(fingerTouchDelta.y))
@@ -38,12 +70,13 @@ public class MoveBubble : MonoBehaviour
             //move right
             if (fingerTouchDelta.x > 0)
             {
-                transform.position += new Vector3(_movementAmount, 0, 0);
+                _goToPosition += new Vector3(_movementAmount, 0, 0);
             }
+
             //move left
             else
             {
-                transform.position += new Vector3(-_movementAmount, 0, 0);
+                _goToPosition += new Vector3(-_movementAmount, 0, 0);
             }
         }
 
@@ -53,14 +86,18 @@ public class MoveBubble : MonoBehaviour
             //move up
             if (fingerTouchDelta.y > 0)
             {
-                transform.position += new Vector3(0, _movementAmount, 0);
+                _goToPosition += new Vector3(0, _movementAmount, 0);
             }
             //move down
             else
             {
-                transform.position += new Vector3(0, -_movementAmount, 0);
+                _goToPosition += new Vector3(0, -_movementAmount, 0);
             }
         }
+
+
+        StartCoroutine(MoveToPosition());
+        _isMoving = true;
 
         Debug.Log("up");
     }
@@ -72,15 +109,6 @@ public class MoveBubble : MonoBehaviour
 
     private void Touch_onFingerMove(Etouch.Finger finger)
     {
-            Debug.Log("move");
-    }
-
-    private void OnEnable()
-    {
-        Etouch.EnhancedTouchSupport.Enable();
-    }
-    private void OnDisable()
-    {
-        Etouch.EnhancedTouchSupport.Disable();
+        Debug.Log("move");
     }
 }
