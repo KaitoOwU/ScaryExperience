3a15218f65efeaffd2e681a229f08229b8d49086
Kaito <advestankv@gmail.com>
2023-06-09 16:29:35 +0200

    FIX • Small Quality Improvements

--- Assets/Scripts/Save/SaveData.cs
+++ Assets/Scripts/Save/SaveData.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class SaveData
 {
     public Dictionary<int, LevelData> levelData;
+    public string version;
 
     public SaveData(Dictionary<int, LevelData> levelData)
     {
         this.levelData = levelData;
+        this.version = Application.version;
     }
 }
