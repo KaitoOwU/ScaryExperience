d034756c326db319a52444e808241ccc0f855391
Alexis Le Bourhis <lebourhisalexis11@gmail.com>
2023-06-10 18:14:01 +0200

    FIX • Light Character

--- Assets/Scripts/FlameManager.cs
+++ Assets/Scripts/FlameManager.cs
@@ -78,7 +78,7 @@ public class FlameManager : MonoBehaviour
         else
         {
             DOTween.To(() => _light.pointLightOuterRadius, x => _light.pointLightOuterRadius = x, _maxSizeLight - (_maxSizeLight / 14) * (10 - _value), 0.5f).SetEase(Ease.OutExpo);
-            DOTween.To(() => _light.intensity, x => _light.intensity = x, 1.5f + (0.15f * _value), 0.5f).SetEase(Ease.OutExpo);
+            /*DOTween.To(() => _light.intensity, x => _light.intensity = x, 1.5f + (0.15f * _value), 0.5f).SetEase(Ease.OutExpo);*/
         }
         
         _light.color = new Color(Mathf.Lerp(_minColor.r, _maxColor.r, _value / 10), Mathf.Lerp(_minColor.g, _maxColor.g, _value / 10), Mathf.Lerp(_minColor.b, _maxColor.b, _value / 10));
