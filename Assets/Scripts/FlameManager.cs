51b83fee3acc0b3af0aa8393f0f5d9de833a00b2
Kaito <advestankv@gmail.com>
2023-06-10 17:58:14 +0200

    Auto stash before merge of "gp" and "origin/gp"

--- Assets/Scripts/FlameManager.cs
+++ Assets/Scripts/FlameManager.cs
@@ -5,6 +5,7 @@ using DG.Tweening;
 using NaughtyAttributes;
 using UnityEngine.Rendering.Universal;
 using UnityEditor;
+using TMPro;
 
 public class FlameManager : MonoBehaviour
 {
@@ -14,6 +15,9 @@ public class FlameManager : MonoBehaviour
     [SerializeField] Light2D _light;
     [SerializeField] Color _maxColor;
     [SerializeField] Color _minColor;
+
+    [SerializeField] Transform _playerCanva;
+    [SerializeField] GameObject _indicateur;
 
     [SerializeField] CinemachineVirtualCamera _vCam;
     CinemachineBasicMultiChannelPerlin _noise;
@@ -48,14 +52,16 @@ public class FlameManager : MonoBehaviour
         if (substract)
         {
             _value -= amount;
-            
         }
         else 
         {
             _value += amount;
+            TextMeshProUGUI text = Instantiate(_indicateur, _playerCanva).GetComponent<TextMeshProUGUI>();
+            text.text = "+" + amount;
+            text.DOScale(1, 0f).SetEase(Ease.OutExpo);
+            text.transform.DOLocalMoveY(text.transform.localPosition.y + 1f, 2f).SetEase(Ease.OutExpo);
+            text.DOColor(new(1, 1, 1, 0), 2f).SetEase(Ease.OutExpo);
             monsterSpawn.RefreshCircle();
-            
-            
         }
         _value = Mathf.Clamp(_value, 0, _max);
         if(_value == 0)
